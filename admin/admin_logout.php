d9263d69fb43d812e379c88218d8c0fc17e563c3
nevilparmar11 <nevilparmar24@gmail.com>
2020-10-17 13:02:56 +0530

    delete all

--- admin/admin_logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include('auth.php');
-session_destroy();
-header('Location:admin_login.php?msg=logout');
-die();
-?>
--- admin/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if(!isset($_SESSION['user'])){
-        header('Location: admin_login.php?msg=unauth');
-		die();
-    }
-
-?>
